b1445089a3f74ab972cfdfe48eea220f4dd207d4
masterteedy <masterteedy@gmail.com>
2012-07-05 21:37:48 -0600

    Initial. Basic commands

---
+++ onkyoserial.py
@@ -0,0 +1,25 @@
+import serial
+
+class onkyoserial:
+  def __init__(self, serport):
+    self.ser = serial.Serial(port=serport,baudrate=9600,timeout=0,rtscts=0,xonxoff=0)
+
+  def ser_cmd(self, cmd):
+    if self.ser.isOpen():
+      self.ser.write(''.join(['!', cmd, '\r']))
+
+  def pwr_on(self):
+    """System power on."""
+    self.ser_cmd('PWR01')
+
+  def pwr_off(self):
+    """System power off."""
+    self.ser_cmd('PWR00')
+
+  def vol_up(self):
+    """Main volume up."""
+    self.ser_cmd('MVLUP')
+
+  def vol_down(self):
+    """Main volume down."""
+    self.ser_cmd('MVLDOWN')
